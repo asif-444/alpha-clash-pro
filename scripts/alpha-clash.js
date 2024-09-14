01dfcfeec8e00e7f4f5c2e74a7465cd5bd99134a
asif-444 <alzabinasif779@gmail.com>
2024-09-14 12:27:53 +0600

    got random alphabet

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,7 +10,22 @@
 //     // console.log(playgroundSection.classList)
 // }
 
-function play(){
+
+function continueGame() {
+    // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBgColorById(alphabet);
+}
+
+function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
